a6dfd08a6beb93876d2f7304b90d6bff7ab3b6c1
titi2274 <tiagoatanazio28@gmail.com>
2026-04-19 21:58:44 -0300

    Criando lista de cards e trocando dados atraves de props

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
-type Props = {}
 
-const Card = (props: Props) => {
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
+
+const Card = ({ companyName, ticker, price }: Props) => {
   return (
     <div className="card">
       <img
         src="https://images.unsplash.com/photo-1612428978260-2b9c7df20150?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
         alt="Image"
       />
-
-        <div className="details">
-            <h2>AAPL</h2>
-            <p>$110</p>
-
-        </div>
-        <p className="info">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, explicabo.</p>
+      <div className="details">
+        <h2>
+          {companyName} ({ticker})
+        </h2>
+        <p>${price}</p>
+      </div>
+      <p className="info">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni,
+        officia!
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
