be03a58de017b0d4c360a16d075bc6d7043439f7
titi2274 <tiagoatanazio28@gmail.com>
2026-04-26 12:17:18 -0300

    Adicionando componentes de função e seus tipos

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { JSX } from "react";
 import "./Card.css";
+  
 
 
 interface Props {
@@ -8,7 +9,7 @@ interface Props {
   price: number;
 }
 
-const Card = ({ companyName, ticker, price }: Props) => {
+const Card: React.FC<Props> = ({companyName, ticker, price,}: Props): JSX.Element => {
   return (
     <div className="card">
       <img
--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { JSX } from "react";
 import Card from "../Card/Card";
 
 interface Props {}
 
-const CardList = (props: Props) => {
+const CardList: React.FC<Props> = (props: Props): JSX.Element => {
   return (
     <div>
       <Card companyName="Apple" ticker="AAPL" price={100} />
