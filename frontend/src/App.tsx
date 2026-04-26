2a789c1ebe09b31210feeb021bf3fc623ca8dba7
titi2274 <tiagoatanazio28@gmail.com>
2026-04-26 15:44:48 -0300

    Recebendo e tipando os dados da API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,28 @@ import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
     const [search, setSearch] = useState<string>("");
+    const [searchResult, setSearchResult] = useState<CompanySearch[]>();
+    const [serverError, setServerError] = useState<string>(""); ;
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
     console.log(e);
   };
 
-    const onClick = (e: SyntheticEvent) => {
-    console.log(e);
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    //setServerError(result.data);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data.data)) {
+      setSearchResult(result.data.data);
+    }
+    console.log(searchResult);
   };
   return (
     <div className="App">
