a1ceeacc2892321878e93b77e0362df26264031c
titi2274 <tiagoatanazio28@gmail.com>
2026-04-26 16:48:52 -0300

    aplicando o forms para adicionar acoes ao portfolio

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,15 +1,17 @@
-import React, { JSX } from "react";
+import React, { JSX, SyntheticEvent } from "react";
 import "./Card.css";
 import { CompanySearch } from "../../company";
+import AddPortfolio from "../Portfolio/AddPortfolio/AddPortfolio";
   
 
 
 interface Props {
   id: string;
   searchResult: CompanySearch;
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({ id, searchResult }: Props): JSX.Element => {
+const Card: React.FC<Props> = ({ id, searchResult, onPortfolioCreate }: Props): JSX.Element => {
   return (
     <div className="card">
       <img alt="company logo"/>
@@ -22,6 +24,11 @@ const Card: React.FC<Props> = ({ id, searchResult }: Props): JSX.Element => {
       <p className="info">
         {searchResult.exchangeShortName} - {searchResult.stockExchange}
       </p>
+
+      <AddPortfolio
+        onPortfolioCreate={onPortfolioCreate}
+        symbol={searchResult.symbol}
+      />
     </div>
   );
 };
