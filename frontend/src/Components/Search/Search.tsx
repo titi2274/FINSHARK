1d93ef5a22346a69ec046b3eb4d9b03f8a328d13
titi2274 <tiagoatanazio28@gmail.com>
2026-04-26 13:34:29 -0300

    Refatorando o fluxo de dados do search com o app

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,24 +1,20 @@
 import React, { ChangeEvent, JSX, SyntheticEvent, useState } from "react";
 
-type Props = {};
+interface Props {
+  onClick: (e: SyntheticEvent) => void;
+  search: string | undefined;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
 
-const Search: React.FC<Props> = (props: Props): JSX.Element => {
-  const [search, setSearch] = useState<string>("");
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e);
-  };
-
-    const onClick = (e: SyntheticEvent) => {
-    console.log(e);
-  };
-
-
+const Search: React.FC<Props> = ({
+  onClick,
+  search,
+  handleChange,
+}: Props): JSX.Element => {
   return (
     <div>
-        <input value={search} onChange={(e) => handleChange(e)}></input>
-        <button onClick={(e) => onClick(e)} />
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+      <button onClick={(e) => onClick(e)} />
     </div>
 
   );
