d345ec00e26bdb5f3826d0f01cee04a6c4bebe64
titi2274 <tiagoatanazio28@gmail.com>
2026-04-30 22:36:05 -0300

    Adicionando comparador de ações

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -5,6 +5,7 @@ import Sidebar from "../../Components/Sidebar/Sidebar";
 import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
 import Tile from "../../Components/Tile/Tile";
 import Spinner from "../../Components/Spinner/Spinner";
+import CompFinder from "../../Components/CompFinder/CompFinder";
 
 interface Props { }
 
@@ -42,10 +43,14 @@ const CompanyPage = (props: Props) => {
             <Tile title="Price" subTitle={company.price.toString()} />
             <Tile title="Sector" subTitle={company.sector} />
             <Tile title="Market Cap" subTitle={company.mktCap.toString()} />
+            <CompFinder ticker={company.symbol} />
+            <p className="bg-white shadow rounded text-medium font-medium text-gray-900 p-3 mt-1 m-4">
+              {company.description}
+            </p>
           </CompanyDashboard>
         </div>
       ) : (
-        <Spinner/>
+        <Spinner />
       )}
     </>
   );
