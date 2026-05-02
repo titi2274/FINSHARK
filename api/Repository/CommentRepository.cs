ccaa5b3ef1423d3881e9fc893a5cf595f4ff6ea6
titi2274 <tiagoatanazio28@gmail.com>
2026-05-02 12:58:09 -0300

    Implementando o delete de comentário

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -13,6 +13,20 @@ namespace api.Repository
         {
             _context = context;
         }
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var commentModel = await _context.Comments.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (commentModel == null)
+            {
+                return null;
+            }
+
+            _context.Comments.Remove(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+        }
+
         public async Task<List<Comment>> GetAllAsync()
         {
             return await _context.Comments.ToListAsync();
