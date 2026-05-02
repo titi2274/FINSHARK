12ac2f67761a085e4724ec38b7365b2bce64059f
titi2274 <tiagoatanazio28@gmail.com>
2026-05-02 11:49:07 -0300

    Refatorando para o repositório

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using api.Dtos.Stock;
 
 namespace api.Interfaces
 {
     public interface IStockRepository
     {
         Task<List<Stock>> GetAllAsync();
+        Task<Stock?> GetByIdAsync(int id);
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+        Task<Stock?> DeleteAsync(int id);
     }
 }
