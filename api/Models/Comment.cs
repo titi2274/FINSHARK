f11630eeb25d6a5a306c5af29127a3223e640589
titi2274 <tiagoatanazio28@gmail.com>
2026-05-03 17:45:20 -0300

    Implementando relacionamento de um para um no model do comment

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace api.Models
 {
@@ -15,5 +15,7 @@ namespace api.Models
         public DateTime CreatedOn { get; set; } = DateTime.Now;
         public int? StockId { get; set; }
         public Stock? Stock { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
