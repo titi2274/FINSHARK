dc4e800cd48a4da268318ccae201f0ad92e4b6c2
titi2274 <tiagoatanazio28@gmail.com>
2026-05-03 18:11:22 -0300

    Terminando a API

--- api/Interfaces/ICommentRepository.cs
+++ api/Interfaces/ICommentRepository.cs
@@ -2,16 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Helpers;
 using api.Models;
 
 namespace api.Interfaces
 {
     public interface ICommentRepository
     {
-        Task<List<Comment>> GetAllAsync();
+        Task<List<Comment>> GetAllAsync(CommentQueryObject queryObject);
         Task<Comment?> GetByIdAsync(int id);
         Task<Comment> CreateAsync(Comment commentModel);
-        Task<Comment?> DeleteAsync(int id);
         Task<Comment?> UpdateAsync(int id, Comment commentModel);
+        Task<Comment?> DeleteAsync(int id);
     }
 }
